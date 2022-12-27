a9d677059195b88008eb2a8181b47e2a98d5c9c6
) Iyamba <mac@MACs-MBP.lan>
2022-12-27 11:22:08 +0100

    Add Task model and controller

---
+++ database/migrations/2022_12_27_101615_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('user_id')->constrained();
+            $table->date('deadline');
+            $table->boolean('status');
+            $table->boolean('priority');
+            $table->string('file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
